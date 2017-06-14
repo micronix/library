4232e31825fea2f16dea74dd44946084a2355f6c
Code the Dream <dan@unitingnc.org>
2017-06-14 00:38:48 +0000

    add book

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def new
+  end
+  
+  def create
+      book = Book.new
+      book.title = params['title']
+      book.description = params['description']
+      book.year = params['year']
+      book.isbn = params['isbn']
+      book.author = params['author']
+      book.genre = params['genre']
+      book.location = params['location']
+      book.save
+      redirect_to '/book'
+  end
 end
