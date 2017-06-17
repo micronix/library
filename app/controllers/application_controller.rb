d20e6293737379731dda73941e8963dad7985c6a
Code the Dream <dan@unitingnc.org>
2017-06-17 16:08:52 +0000

    add edit ability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,38 @@ class ApplicationController < ActionController::Base
       book.author = params['author']
       book.genre = params['genre']
       book.location = params['location']
+      book.image = params['image']
       book.save
-      redirect_to '/book'
+      redirect_to '/'
+  end
+  
+  def search_form
+  end
+  
+  def search
+    title = params[:title]
+    
+    @books = Book.where('title LIKE ?',"%#{title}%")
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    
+    @book.title = params['title']
+    @book.description = params['description']
+    @book.year = params['year']
+    @book.isbn = params['isbn']
+    @book.author = params['author']
+    @book.genre = params['genre']
+    @book.location = params['location']
+    @book.image = params['image']
+    
+    @book.save
+    
+    redirect_to '/'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610145614) do
+ActiveRecord::Schema.define(version: 20170617145616) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170610145614) do
     t.string "author"
     t.string "genre"
     t.string "location"
+    t.string "image"
   end
 
 end
